bee4c1c1da8d4e44b9b86d675aad747ade4dd99b
xkevinpham <57520301+xkevinpham@users.noreply.github.com>
2020-03-01 22:46:19 -0800

    updated app,.s with more components

--- react-training/reactTraining/src/app.js
+++ react-training/reactTraining/src/app.js
@@ -25,7 +25,40 @@ class App extends Component {
 
         <h1>Greetings</h1>
         {<Greetings lang="de">Ludwig</Greetings>
-<Greetings lang="fr">François</Greetings>}
+<Greetings lang="fr">François</Greetings>
+<BoxColor r={255} g={0} b={0} />
+<BoxColor r={128} g={255} b={0} />
+}
+
+{
+  <CreditCard 
+  type="Visa"
+  number="0123456789018845"
+  expirationMonth={3}
+  expirationYear={2021}
+  bank="BNP"
+  owner="Maxence Bouret"
+  bgColor="#11aa99"
+  color="white" />
+<CreditCard 
+  type="Master Card"
+  number="0123456789010995"
+  expirationMonth={3}
+  expirationYear={2021}
+  bank="N26"
+  owner="Maxence Bouret"
+  bgColor="#eeeeee"
+  color="#222222" />
+<CreditCard 
+  type="Visa"
+  number="0123456789016984"
+  expirationMonth={12}
+  expirationYear={2019}
+  bank="Name of the Bank"
+  owner="Firstname Lastname"
+  bgColor="#ddbb55"
+  color="white" />
+}
       </div>
     );
   }
