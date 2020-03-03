57a5772551dcf95a0c051113381e43bb796fbe7d
xkevinpham <57520301+xkevinpham@users.noreply.github.com>
2020-03-03 08:10:16 -0800

    creating star ratings in js

--- react-training/reactTraining/src/app.js
+++ react-training/reactTraining/src/app.js
@@ -59,6 +59,17 @@ class App extends Component {
   bgColor="#ddbb55"
   color="white" />
 }
+
+{
+  <Rating>0</Rating>
+  <Rating>1.49</Rating>
+  <Rating>1.5</Rating>
+  <Rating>3</Rating>
+  <Rating>4</Rating>
+  <Rating>5</Rating>
+}
+
+
       </div>
     );
   }
